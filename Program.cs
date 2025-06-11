316f004a6175bd93143d268fc4701cec32dc771d
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-06-10 22:23:17 -0300

    add :00

--- Program.cs
+++ Program.cs
@@ -7,19 +7,17 @@ int jogadas = Convert.ToInt32(Console.ReadLine());
 
 int[,] surpresinha = new int[jogadas, 6];
 
-Random gerador = new Random();
+Random gerador = new Random(); //gerador de nm aleatórios
 
 for (int i0 = 0; i0 < jogadas; i0++)
 {
+    
     for (int i1 = surpresinha.GetLowerBound(1); i1 <= surpresinha.GetUpperBound(1); i1++)
     {
-        surpresinha[i0, i1] = gerador.Next(1, 60);
-        Console.Write($"[{surpresinha[i0, i1]}] ");
+        surpresinha[i0, i1] = gerador.Next(01, 60);
+        
+        Console.Write($"[{surpresinha[i0, i1]:00}] "); // :00 para exibir dois dígitos
     }
-    Console.WriteLine("");
+    Console.WriteLine();
 }
-
-// foreach (int surpresa in surpresinha)
-// {
-//     Console.Write($"[{surpresa}] ");
-// }
+// Array.Sort(surpresinha); - Organizar
